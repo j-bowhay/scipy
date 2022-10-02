365fdf81476603084edb46beb6a6ae4c69d40099
Jake Bowhay <jb9.bowhay@gmail.com>
2022-10-02 17:07:58 +0100

    DEP: remove deprecated sort keyword from qz decomposition

--- scipy/linalg/_decomp_qz.py
+++ scipy/linalg/_decomp_qz.py
@@ -67,13 +67,8 @@ def _ouc(x, y):
     return out
 
 
-def _qz(A, B, output='real', lwork=None, sort=None, overwrite_a=False,
+def _qz(A, B, output='real', lwork=None, overwrite_a=False,
         overwrite_b=False, check_finite=True):
-    if sort is not None:
-        # Disabled due to segfaults on win32, see ticket 1717.
-        raise ValueError("The 'sort' input of qz() has to be None and will be "
-                         "removed in a future release. Use ordqz instead.")
-
     if output not in ['real', 'complex', 'r', 'c']:
         raise ValueError("argument must be 'real', or 'complex'")
 
@@ -141,7 +136,7 @@ def _qz(A, B, output='real', lwork=None, sort=None, overwrite_a=False,
     return result, gges.typecode
 
 
-def qz(A, B, output='real', lwork=None, sort=None, overwrite_a=False,
+def qz(A, B, output='real', lwork=None, overwrite_a=False,
        overwrite_b=False, check_finite=True):
     """
     QZ decomposition for generalized eigenvalues of a pair of matrices.
@@ -177,25 +172,6 @@ def qz(A, B, output='real', lwork=None, sort=None, overwrite_a=False,
         Default is 'real'.
     lwork : int, optional
         Work array size. If None or -1, it is automatically computed.
-    sort : {None, callable, 'lhp', 'rhp', 'iuc', 'ouc'}, optional
-        NOTE: THIS INPUT IS DISABLED FOR NOW. Use ordqz instead.
-
-        Specifies whether the upper eigenvalues should be sorted. A callable
-        may be passed that, given a eigenvalue, returns a boolean denoting
-        whether the eigenvalue should be sorted to the top-left (True). For
-        real matrix pairs, the sort function takes three real arguments
-        (alphar, alphai, beta). The eigenvalue
-        ``x = (alphar + alphai*1j)/beta``. For complex matrix pairs or
-        output='complex', the sort function takes two complex arguments
-        (alpha, beta). The eigenvalue ``x = (alpha/beta)``.  Alternatively,
-        string parameters may be used:
-
-            - 'lhp'   Left-hand plane (x.real < 0.0)
-            - 'rhp'   Right-hand plane (x.real > 0.0)
-            - 'iuc'   Inside the unit circle (x*x.conjugate() < 1.0)
-            - 'ouc'   Outside the unit circle (x*x.conjugate() > 1.0)
-
-        Defaults to None (no sorting).
     overwrite_a : bool, optional
         Whether to overwrite data in a (may improve performance)
     overwrite_b : bool, optional
@@ -256,7 +232,7 @@ def qz(A, B, output='real', lwork=None, sort=None, overwrite_a=False,
     # AA, BB, sdim, alphar, alphai, beta, vsl, vsr, work, info
     # output for complex
     # AA, BB, sdim, alpha, beta, vsl, vsr, work, info
-    result, _ = _qz(A, B, output=output, lwork=lwork, sort=sort,
+    result, _ = _qz(A, B, output=output, lwork=lwork,
                     overwrite_a=overwrite_a, overwrite_b=overwrite_b,
                     check_finite=check_finite)
     return result[0], result[1], result[-4], result[-3]
@@ -352,7 +328,7 @@ def ordqz(A, B, sort='lhp', output='real', overwrite_a=False,
     array([ True,  True, False, False], dtype=bool)
 
     """
-    (AA, BB, _, *ab, Q, Z, _, _), typ = _qz(A, B, output=output, sort=None,
+    (AA, BB, _, *ab, Q, Z, _, _), typ = _qz(A, B, output=output,
                                             overwrite_a=overwrite_a,
                                             overwrite_b=overwrite_b,
                                             check_finite=check_finite)
